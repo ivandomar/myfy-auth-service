dc4c3370a686665530a5568b15d0da7b15475ae3
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-09-25 22:07:38 -0300

    feature(models): adds PK generation

--- models/token.py
+++ models/token.py
@@ -1,5 +1,6 @@
 import hashlib
 import time
+import uuid
 
 from datetime import datetime
 from sqlalchemy import Column, DateTime, ForeignKey, String
@@ -11,6 +12,7 @@ from .base import Base
 class Token(Base):
     __tablename__ = 'token'
 
+    id = Column("id", String(36), primary_key=True)
     user_id = Column(String(36), ForeignKey("user.id"), nullable=False)
     content = Column(String(64), nullable=False)
     expiration_date = Column(DateTime, nullable=False)
@@ -19,6 +21,7 @@ class Token(Base):
     deleted_at = Column(DateTime)
 
     def __init__(self, user_id:str):
+        self.id = str(uuid.uuid4())
         self.user_id = user_id
 
         current_time = time.time()
--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 import hashlib
+import uuid
 
 from datetime import datetime
 from sqlalchemy import Column, DateTime, String
@@ -22,6 +23,7 @@ class User(Base):
     children = relationship("Token")
 
     def __init__(self, name:str, email:str, login:str, password:str):
+        self.id = str(uuid.uuid4())
         self.name = name
         self.email = email
         self.login = login
